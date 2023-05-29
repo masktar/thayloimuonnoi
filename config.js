de38e9abadffa9d6f1eb3f39864a4b0ad88dc117
masktar <110052237+masktar@users.noreply.github.com>
2023-05-29 22:17:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Tại vì bạn cười chu môi xinh á ~~',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Hẹn lúc nào đó gặp em nhá :33',
     btnAccept: 'Okiiiii lun <3',
